d6c16882eaa82688a22e858c7168257b272f2479
adlascio <arthur.dilascio@gmail.com>
2022-05-02 23:40:39 -0700

    first commit with exercises, no solution

---
+++ exercises/ex1.js
@@ -0,0 +1,16 @@
+/*
+Write an if…else statement that:
+- Prints “even” if the number is an even number
+- Prints “odd” if the number is an odd number
+
+Hint: Use the %(modulo) operator to determine is a number is even or odd.
+*/
+
+const number = 2; // Change your number to test your if statement
+
+if (/* your conditional goes here */) {
+
+} else {
+
+}
+
